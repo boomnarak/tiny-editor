7296105cf16430e45f33752094bb7360e83090cb
boom <sutthiphong@terrabit.co.th>
2020-06-25 18:02:37 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <editor v-model="text" 
+        api-key="qagffr3pkuv17a8on1afax661irst1hbr4e6tbv888sz91jc"
+       :init="{
+         height: 500,
+         plugins: [
+           'advlist autolink lists link image charmap print preview anchor',
+           'searchreplace visualblocks code fullscreen',
+           'insertdatetime media table paste code help wordcount'
+         ],
+         toolbar:
+           'undo redo | formatselect | bold italic backcolor | \
+           alignleft aligncenter alignright alignjustify | \
+           bullist numlist outdent indent | removeformat | help |\
+           table tabledelete | tableprops tablerowprops tablecellprops | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol',
+       }"></editor>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Editor from '@tinymce/tinymce-vue';
 export default {
   name: 'App',
+  data() {
+    return {
+      text: ''
+    }
+  },
   components: {
-    HelloWorld
-  }
+    'editor': Editor // <- Important part
+  },
 }
 </script>
 
